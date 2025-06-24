21db130a66b3d52f4310078a8e86fde512f77026
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2025-06-24 11:00:06 +0200

    feat: 🔧 temporary uuidv4 feature to test the bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use pyo3::prelude::*;
+use uuid::Uuid;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -6,9 +7,16 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+/// Returns a random UUID v4
+#[pyfunction]
+fn random_uuid_v4() -> Uuid {
+    Uuid::new_v4()
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn uuid_blake3(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(random_uuid_v4, m)?)?;
     Ok(())
 }
